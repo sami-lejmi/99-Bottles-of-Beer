29c704711e3998426f29eb18a8e315291e69dcf2
Sami Lejmi <96699019+sami-lejmi@users.noreply.github.com>
2022-02-05 09:50:07 +0100

    Update bottles-99.py

--- bottles-99.py
+++ bottles-99.py
@@ -3,10 +3,10 @@
 for x in range(99, 0, -1):
     if x > 2:
         print(x, "Bottles of beer on the wall,", x, "bottles of beer.")
-        print("Take one down and pass it around,", x - 1, "bottles of beer on the wall")
+        print("Take one down and pass it around,", x - 1, "bottles of beer on the wall.")
     if x == 2:
         print(x, "Bottles of beer on the wall,", x, "bottles of beer.")
-        print("Take one down and pass it around,", x - 1, "bottle of beer on the wall")
+        print("Take one down and pass it around,", x - 1, "bottle of beer on the wall.")
     if x == 1:
         print(x, "Bottle of beer on the wall,", x, "bottle of beer.")
         print("Take one down and pass it around, no more bottles of beer on the wall.")
